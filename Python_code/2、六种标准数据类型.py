843f5aef496308b3f3002a01207c47a700c5d307
ouyangrb <519222620@qq.com>
2024-02-26 12:03:26 +0800

    Initial commit

--- Python_code/2、六种标准数据类型.py
+++ Python_code/2、六种标准数据类型.py
@@ -1,3 +1,16 @@
-print(111)
+# print(r'www.abc./dd/ddd')  # 加一个r里面不会发生转义
 
-print('ok')
+num1 = 1.0
+print(type(num1))  # <class 'float'>查看类型
+
+# 关于整数的强制转换 int([x],base=10)
+print(int())  # 0
+print(int('123'))  # 123
+print(int('10101', 2))  # 21
+
+# 除了数字0，空字符串，空列表，空字典，空元组，空集合，关键字（False和None）判定为False，其他都为True
+# 关键字None的意思是什么都没有
+print(bool())  # False
+print(bool(0))  # False
+print(bool(None))  # False
+
---
+++ Python_code/3、输入和输出.py
@@ -0,0 +1,9 @@
+# rec = input()  # 先输入再赋值
+# print(rec)
+# rec2 = input('qingshuru:\n')
+
+# print(1, 2, 3, 4)  # 1 2 3 4
+# print(1, 2, 3, 4, sep='-')  # 1-2-3-4
+# with open ("./abc.txt", 'w') as f:  # ./表示当前目录，以写入的方式，打开文件对象
+#     print(12345, file=f)  # print里面的东西写在f里面
+
