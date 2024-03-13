4a82db08708ccfcf0b41a548b7482fecce24446e
ouyangrb <519222620@qq.com>
2024-03-13 17:14:21 +0800

    Initial commit

--- CV_Project/项目1：图像分类迁移学习-猫狗数据集/vgg_googlenet_resnet_predict.py
+++ CV_Project/项目1：图像分类迁移学习-猫狗数据集/vgg_googlenet_resnet_predict.py
@@ -1,17 +1,20 @@
 import torch
 from PIL import Image
-from torchvision import models,transforms
-from torchvision.models import VGG16_BN_Weights, GoogLeNet_Weights, ResNet50_Weights
+from torchvision import models, transforms
+from torchvision.models import VGG16_BN_Weights, GoogLeNet_Weights, ResNet50_Weights, ResNet18_Weights
 import torch.nn as nn
 
 
 if __name__ == '__main__':
-
-    vgg = models.vgg16_bn(weights=VGG16_BN_Weights.IMAGENET1K_V1)
+    '''
+    图片分类学一个的话，学resnet50，其次vgg16_bn，再次googlenet
+    '''
+    vgg = models.vgg16_bn(weights=VGG16_BN_Weights.IMAGENET1K_V1)  # 在imagenet数据集上训练好的模型
     googlenet = models.googlenet(weights=GoogLeNet_Weights.IMAGENET1K_V1)
-    resnet = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+    resnet50 = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+    resnet18 = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 
-    print(resnet)
+    print(resnet50)
     # model = resnet
     # model.eval()
     #
